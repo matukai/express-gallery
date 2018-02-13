670315f773844cbf932c94d28aac0e67dfd3d52f
matukai <matt.tanji@gmail.com>
2018-02-12 14:13:11 -1000

    cleaning for new branch

--- knex/migrations/20180209164607_create_gallery_table.js
+++ knex/migrations/20180209164607_create_gallery_table.js
@@ -5,11 +5,11 @@ exports.up = function(knex, Promise) {
     table.string('author').notNullable();
     table.string('link').notNullable();
     table.string('description').notNullable();
-    table.timestamp('created_at').defaultTo(knex.fn.now())
-    table.timestamp('updated_at').defaultTo(knex.fn.now())
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
   })
 };
-
+ 
 exports.down = function(knex, Promise) {
   return knex.schema.dropTable('gallery');
 };
