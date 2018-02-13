3bac5980db4a0ce580b49caaabe061217ec9be87
matukai <matt.tanji@gmail.com>
2018-02-12 14:20:56 -1000

    updated table description to hold more than 255 string text

--- knex/migrations/20180209164607_create_gallery_table.js
+++ knex/migrations/20180209164607_create_gallery_table.js
@@ -4,12 +4,12 @@ exports.up = function(knex, Promise) {
     table.increments();
     table.string('author').notNullable();
     table.string('link').notNullable();
-    table.string('description').notNullable();
+    table.text('description').notNullable();
     table.timestamp('created_at').defaultTo(knex.fn.now());
     table.timestamp('updated_at').defaultTo(knex.fn.now());
   })
 };
- 
+
 exports.down = function(knex, Promise) {
   return knex.schema.dropTable('gallery');
 };
